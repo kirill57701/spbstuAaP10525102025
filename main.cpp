e16c1ef71b0728dcaa084cccea56419ca39f5a72
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-10-25 12:58:32 +0300

    init commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <cstddef>
 
+unsigned ispyth(unsigned a, unsigned b, unsigned c){
+  if ((a*a + b*b == c*c) || (a*a + c*c == b*b) || (c*c + b*b == a*a)){retrun 1;}else{return 0;}
+}
+
 int main(){
 using u_t = unsigned;
-u_t a = 0;
+u_t a = 0, b = 0, c = 0;
 std::cin >> a;
 size_t count = 0;
 while (std::cin >> a){
-  count += a*a;
+  count += ispyth(a, b, c);
 }
 
 if (std::cin.eof()){ 
