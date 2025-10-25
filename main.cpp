104a3e0c2b40d49cf2816cd3ee867de182487698
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-10-25 13:10:24 +0300

    Pifagortriangle

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include <cstddef>
 
 unsigned ispyth(unsigned a, unsigned b, unsigned c){
-  if ((a*a + b*b == c*c) || (a*a + c*c == b*b) || (c*c + b*b == a*a)){retrun 1;}else{return 0;}
+  if ((a*a + b*b == c*c) || (a*a + c*c == b*b) || (c*c + b*b == a*a)){return 1;}else{return 0;}
 }
 
 int main(){
 using u_t = unsigned;
 u_t a = 0, b = 0, c = 0;
-std::cin >> a;
 size_t count = 0;
 while (std::cin >> a){
   count += ispyth(a, b, c);
+  c = b;
+  b = a;
 }
 
 if (std::cin.eof()){ 
