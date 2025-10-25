8d2bfed6584a3d07575c116c58d1b9c5f0141b55
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-10-25 12:43:34 +0300

    newchange

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstddef>
 
 int main(){
 using u_t = unsigned;
 u_t a = 0;
 std::cin >> a;
-
+size_t count = 0;
+while (std::cin >> a){
+  count++;
+}
 
 if (std::cin.eof()){ 
   std::cout << 0;
